d60751aef27de7e63b94cdefd19bda235aa28c0a
Myat Thiha Naing <myatthihanaing.mthn@gmail.com>
2024-09-29 23:32:02 +0800

    added deletion

--- src/firebase/firebaseUtils.js
+++ src/firebase/firebaseUtils.js
@@ -1,5 +1,6 @@
 import { collection, getCountFromServer } from "firebase/firestore";
 import { db } from "./firebase";
+import { fetchDocumentById } from "./fetchDocument";
 
 
 export async function getTotalCount(collectionName){
@@ -8,4 +9,14 @@ export async function getTotalCount(collectionName){
         throw new Error("Error getting total count from firebase", error)
     })
     return snapshot.data().count;
+}
+
+export async function deleteDocumet(collectionName, docId) {
+    const doc = await fetchDocumentById(docId, collectionName);
+    try{
+        await doc.ref.delete();
+    }catch(error){
+        console.log("Error deleting document ", error);
+        throw new Error("Error deleting document");
+    }
 }
